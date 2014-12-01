aa734cbb4d0ef3fa84dc27ce7febe3c69a791c4b
Andrei Pak <apak@firstgaming.com>
2014-12-01 09:58:50 +0200

    setup script added

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+for rc_file in vim/.vimrc bash/.bashrc tmux/.tmux.conf zsh/.zshrc; do
+  dotfile=${rc_file##*/} # get the part after the last slash
+  [ -e ~/$dotfile ] && \
+    mv ~/$dotfile ~/$dotfile.backup.`date +%F`.$RANDOM
+
+  ln -s $PWD/$rc_file ~/$dotfile && echo "success: $PWD/$rc_file -> ~/$dotfile"
+
+done
+
